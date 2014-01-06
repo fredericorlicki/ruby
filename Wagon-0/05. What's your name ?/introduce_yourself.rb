cf1afd77953589dae879b492eeb4fe02da5d054b
Mac Mob <Macmob@MacBook-Pro-de-Mac.local>
2014-01-06 18:56:13 +0100

    solution exo 5

--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,1 +1,11 @@
-# your code goes here, introduce yourself !
+ 
+puts "hey Buddy, what's your name?"
+a=gets.chomp
+puts "whats your middle name"
+b=gets.chomp
+puts "what's your last name"
+c=gets.chomp
+puts "Hello, #{a} #{b} #{c} !"
+
+
+puts "hello" + a + b + c + "!"
