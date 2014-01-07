78379c18bc9040e6e62e4337b74f4522738f454c
Mac Mob <Macmob@MacBook-Pro-de-Mac.local>
2014-01-07 12:49:17 +0100

    solution exercice 6

--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -1,6 +1,39 @@
 def stupid_coaching 
-  # your code goes here
+    puts "wake up, it's time to go to work, Son"
+    answer = gets.chomp
+    until answer == "I am going to work right now SIR !"
+        puts "I can feel your motivation son !" if answer == answer.upcase
+  	     if answer == "i'm going to work"
+new_state(answer)
+         else
+old_state(answer)
+        end
+answer=gets.chomp
+    end
+end
+ 
+  		
+def old_state(answer) 
+    if answer.end_with?("?")
+      puts "Silly question, get dressed and go to work !"
+     else
+      puts "I don't care son, get dressed and go to work !"
+    end
+end
+ 
+
+
+ def new_state(answer)
+  puts "good boy ! Take the subway, it will be faster"
+  answer = gets.chomp
+  until answer == "I am going to work right now SIR !" 
+    if answer.end_with?("?")
+      puts "Silly question, take the subway and go to work !"
+    else
+      puts "i don't care, take the subway and go to work !"
+    end
+    answer = gets.chomp
+  end
 end
 
-# Calling the method
-stupid_coaching
+  stupid_coaching
