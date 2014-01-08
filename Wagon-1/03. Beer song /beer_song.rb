9b7e63387e9cb0c252922426ee002d6f611692c2
Mac Mob <Macmob@MacBook-Pro-de-Mac.local>
2014-01-08 16:42:18 +0100

    solution wagon exercice 3

--- Wagon-1/03. Beer song /beer_song.rb
+++ Wagon-1/03. Beer song /beer_song.rb
@@ -1,18 +1,19 @@
 def beersong(nb_at_start)
   # your code goes here
+  
+  return if nb_at_start == 0 
+  case nb_at_start
+    when 1
+      puts "1 bottle of beer on the wall, 1 bottle of beer!"
+   	  puts "Take one down, pass it around, no more bottles of beer on the wall!"
+    when 2
+      puts "2 bottles of beer on the wall, 2 bottles of beer!"
+  	  puts "Take one down, pass it around, 1 bottle of beer on the wall!"
+  	else
+   	  puts "#{nb_at_start} bottles of beer on the wall, #{nb_at_start} bottles of beer!"
+   	  puts "Take one down, pass it around, #{nb_at_start - 1} bottles of beer on the wall!"
+  end
+  beersong(nb_at_start - 1)
 end
 
-beersong( ARGV[0] )
-
-# output (if called with 5 beers) :
-
-# => 5 bottles of beer on the wall, 5 bottles of beer!
-# => Take one down, pass it around, 4 bottles of beer on the wall!
-# => 4 bottles of beer on the wall, 4 bottles of beer!
-# => Take one down, pass it around, 3 bottles of beer on the wall!
-# => 3 bottles of beer on the wall, 3 bottles of beer!
-# => Take one down, pass it around, 2 bottles of beer on the wall!
-# => 2 bottles of beer on the wall, 2 bottles of beer!
-# => Take one down, pass it around, 1 bottle of beer on the wall!
-# => 1 bottle of beer on the wall, 1 bottle of beer!
-# => Take one down, pass it around, no more bottles of beer on the wall!
+beersong( ARGV[0].to_i )
