83c8e99c108f56a63097f8a4c636c6c62a53b9a3
Mac Mob <Macmob@MacBook-Pro-de-Mac.local>
2014-01-09 15:41:57 +0100

    solution exercice 07

--- Wagon-1/07. Enumerable/enumerable.rb
+++ Wagon-1/07. Enumerable/enumerable.rb
@@ -1,32 +1,54 @@
 # TODO: Print the elements at indices 1, 3, 5, 7, etc. on separate lines.
 #       You should make use of Enumerable#each_with_index
 def print_odd_indexed_integers(array)
+	array.each_with_index do |item, index|
+		unless index%2 == 0 
+			puts item
+		end	
+	end
 end
  
 # TODO: Return the odd numbers from a list of integers.
 #       You should make use of Enumerable#select
 def odd_integers(array)
+	array.select do |item,index|
+		if item % 2 != 0 
+	 		print item
+	 		true
+		end
+	end
 end
  
 # TODO: Return the first number from an Array that is less than a particular number - 'limit.'
 #       You should make use of Enumerable#find
 def first_under(array, limit)
+	array.detect do |x| x < limit
+	end
 end
  
 # TODO: Take an Array of Strings and return a new Array with an exclamation point appended to each String.
 #       You should make use of Enumerable#map
 def add_bang(array)
+	array.map do |i|
+		i + "!"
+	end
 end
  
 # TODO: Calculate the sum of an Array of numbers.
 #       You should make use of Enumerable#reduce
 def sum(array)
+	array.reduce(:+)
 end
  
 # TODO: Reorganize an Array of the elements into groups of 3, and then sort each group alphabetically.
 #       You should make use of Enumerable#each_slice
 def sorted_triples(array)
+	res_array = []
+	array.each_slice(3) { |x| res_array << x.sort } 
+	return res_array
 end
+
+
  
 # Driver code... don't change this.
 print_odd_indexed_integers([2, 4, 6, 8, 10, 12]) # this should print "4\n8\n12"
